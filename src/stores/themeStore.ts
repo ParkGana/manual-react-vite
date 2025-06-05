accc7d697fc18efd0288f27ab363b237d6051a22
ParkGana <gnchoco97@gmail.com>
2025-06-05 18:21:10 +0900

    Zustand - persist

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -1,11 +1,17 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type ThemeStoreType = {
   theme: 'light' | 'dark';
   toggleTheme: () => void;
 };
 
-export const useThemeStore = create<ThemeStoreType>((set) => ({
-  theme: 'light',
-  toggleTheme: () => set((state) => ({ theme: state.theme === 'light' ? 'dark' : 'light' }))
-}));
+export const useThemeStore = create(
+  persist<ThemeStoreType>(
+    (set) => ({
+      theme: 'light',
+      toggleTheme: () => set((state) => ({ theme: state.theme === 'light' ? 'dark' : 'light' }))
+    }),
+    { name: 'theme-storage' }
+  )
+);
