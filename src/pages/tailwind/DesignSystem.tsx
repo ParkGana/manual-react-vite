ec2aecc931a5c3d549155e09f1730af3008234f9
ParkGana <gnchoco97@gmail.com>
2025-06-05 10:08:26 +0900

    Tailwind CSS - 반응형

--- src/pages/tailwind/DesignSystem.tsx
+++ src/pages/tailwind/DesignSystem.tsx
@@ -3,7 +3,7 @@ const TailwindDesignSystemPage = () => {
     <div className="flex flex-col gap-10 p-5">
       {/* COLOR */}
       <div className="flex flex-col gap-4 p-4">
-        <p className="text-title-l border-b-2 border-gray-900 p-1">COLOR</p>
+        <p className="text-title3 border-b-2 border-gray-900 p-1">COLOR</p>
         <div className="flex">
           <div className="w-10 h-10 bg-white" />
           <div className="w-10 h-10 bg-black" />
@@ -23,30 +23,26 @@ const TailwindDesignSystemPage = () => {
 
       {/* TYPOGRAPHY */}
       <div className="flex flex-col gap-4 p-4">
-        <p className="text-title-l border-b-2 border-gray-900 p-1">TYPOGRAPHY</p>
+        <p className="text-title3 border-b-2 border-gray-900 p-1">TYPOGRAPHY</p>
         <div>
-          <p className="text-title-2xl">Title/2XL</p>
-          <p className="text-title-xl">Title/XL</p>
-          <p className="text-title-l">Title/L</p>
-          <p className="text-title-m">Title/M</p>
-          <p className="text-title-s">Title/S</p>
+          <p className="text-title1">Title1</p>
+          <p className="text-title2">Title2</p>
+          <p className="text-title3">Title3</p>
+          <p className="text-title4">Title4</p>
         </div>
         <div>
-          <p className="text-body-xl">Body/XL</p>
-          <p className="text-body-l">Body/L</p>
-          <p className="text-body-m">Body/M</p>
-          <p className="text-body-s">Body/S</p>
+          <p className="text-body1">Body1</p>
+          <p className="text-body2">Body2</p>
+          <p className="text-body3">Body3</p>
         </div>
         <div>
-          <p className="text-label-l">Label/L</p>
-          <p className="text-label-m">Label/M</p>
-          <p className="text-label-s">Label/S</p>
-          <p className="text-label-xs">Label/XS</p>
+          <p className="text-label1">Label1</p>
+          <p className="text-label2">Label2</p>
+          <p className="text-label3">Label3</p>
         </div>
         <div>
-          <p className="text-caption-m">Caption/M</p>
-          <p className="text-caption-s">Caption/S</p>
-          <p className="text-caption-xs">Caption/XS</p>
+          <p className="text-caption1">Caption1</p>
+          <p className="text-caption2">Caption2</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,24 +15,6 @@ export default {
         'gray-700': 'var(--gray-700)',
         'gray-800': 'var(--gray-800)',
         'gray-900': 'var(--gray-900)'
-      },
-      fontSize: {
-        'title-2xl': ['40px', { lineHeight: '135%', fontWeight: '700' }],
-        'title-xl': ['24px', { lineHeight: '135%', fontWeight: '700' }],
-        'title-l': ['20px', { lineHeight: '135%', fontWeight: '700' }],
-        'title-m': ['16px', { lineHeight: '135%', fontWeight: '700' }],
-        'title-s': ['12px', { lineHeight: '135%', fontWeight: '700' }],
-        'body-xl': ['24px', { lineHeight: '150%', fontWeight: '500' }],
-        'body-l': ['20px', { lineHeight: '150%', fontWeight: '500' }],
-        'body-m': ['16px', { lineHeight: '150%', fontWeight: '500' }],
-        'body-s': ['12px', { lineHeight: '150%', fontWeight: '500' }],
-        'label-l': ['20px', { lineHeight: '150%', fontWeight: '700' }],
-        'label-m': ['16px', { lineHeight: '150%', fontWeight: '700' }],
-        'label-s': ['12px', { lineHeight: '150%', fontWeight: '700' }],
-        'label-xs': ['10px', { lineHeight: '150%', fontWeight: '700' }],
-        'caption-m': ['16px', { lineHeight: '150%', fontWeight: '500' }],
-        'caption-s': ['12px', { lineHeight: '150%', fontWeight: '500' }],
-        'caption-xs': ['10px', { lineHeight: '150%', fontWeight: '500' }]
       }
     }
   },
